3e53fea1a11b7bac9de4b702d063e729216deb6e
scarecrow95 <mridul@raincrowgames.com>
2022-04-07 00:33:07 -0400

    readded exec stuff

--- server.js
+++ server.js
@@ -36,30 +36,30 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('file'), function (req, res) {
   console.log(req.file.originalname)
-  res.json({
-    Cheeks_Puff: 0.35,
-    Eye_asian: 0.344,
-    Eyebrow_Uni: 0.04,
-    Eyebrow_thin: 0.09,
-    Mouth_size: 0.43,
-    Nose_Fat: 0.8698,
-    Nose_size: 0.5488,
-  })
-  //   exec(
-  //     `python3 ./scripts/infer.py ./images/${req.file.originalname}`,
-  //     (error, stdout, stderr) => {
-  //       if (error) {
-  //         console.log(`error: ${error.message}`)
-  //         return
-  //       }
-  //       if (stderr) {
-  //         console.log(`stderr: ${stderr}`)
-  //         return
-  //       }
-  //       console.log(`stdout: ${stdout}`)
-  //       exec('rm ./images/*', (err, out, serr) => {
-  //         res.json(JSON.stringify(stdout))
-  //       })
-  //     }
-  //   )
+  //   res.json({
+  //     Cheeks_Puff: 0.35,
+  //     Eye_asian: 0.344,
+  //     Eyebrow_Uni: 0.04,
+  //     Eyebrow_thin: 0.09,
+  //     Mouth_size: 0.43,
+  //     Nose_Fat: 0.8698,
+  //     Nose_size: 0.5488,
+  //   })
+  exec(
+    `source /home/ankush/flask_env/bin/activate && python3 ./scripts/infer.py ./images/${req.file.originalname}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      exec('rm ./images/*', (err, out, serr) => {
+        res.json(JSON.stringify(stdout))
+      })
+    }
+  )
 })
